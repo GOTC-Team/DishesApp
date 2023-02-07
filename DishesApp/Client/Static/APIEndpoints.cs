b716e6b4dadc7512b71fbec10957ee4a1a7876c3
sashaFPV <andrei.cazanov2017@gmail.com>
2023-02-07 12:12:50 +0100

    add: basic adding products to user

--- DishesApp/Client/Static/APIEndpoints.cs
+++ DishesApp/Client/Static/APIEndpoints.cs
@@ -9,7 +9,9 @@
 #endif
         internal readonly static string s_register = $"{ServerBaseUrl}/api/user/register";
         internal readonly static string s_signIn = $"{ServerBaseUrl}/api/user/signin";
-        internal readonly static string s_weatherForecast = $"{ServerBaseUrl}/weatherforecast";
         internal readonly static string s_getUsers = $"{ServerBaseUrl}/api/user/get-users";
+        internal readonly static string s_getAllIngredients = $"{ServerBaseUrl}/api/ingredient/get-all-ingredients";
+        internal readonly static string s_getUserIngredients = $"{ServerBaseUrl}/api/ingredient/get-user-ingredients";
+        internal readonly static string s_addIngredientToUser = $"{ServerBaseUrl}/api/ingredient/add-ingredient-to-user";
     }
 }
---
+++ ModelsLibrary/UserNameIngredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLibrary
+{
+    public class UserNameIngredient
+    {
+        public string? UserName { get; set; }
+        public string? IngredientName { get; set; }
+    }
+}
